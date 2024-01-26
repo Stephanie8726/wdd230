bfe170dd949e5a5d0593a857509b2fd386e50d13
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2024-01-25 20:25:18 -0800

    wo3 getdates.js changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -10,5 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const lastModifiedElement = document.getElementById("lastModified");
     const lastModifiedDate = document.lastModified;
     lastModifiedElement.textContent = `Last modified: ${lastModifiedDate}`;
+
+     // code for the hamburger menu
+  const menuButton = document.getElementById('menu-button');
+  const navigation = document.querySelector('.navigation');
+
+  menuButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
   });
+});
   
