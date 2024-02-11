11a6689591e610f795826ef0bd6c95beefe61b3b
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2024-02-11 01:01:41 -0800

    chamber js

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -32,3 +32,44 @@ document.addEventListener("DOMContentLoaded", function () {
   const visitCountPlaceholder = document.getElementById("visitCountPlaceholder");
   visitCountPlaceholder.textContent = `Visits: ${visitCount}`;
 });
+
+// to pin the map
+// function initMap() {
+//   var center = { lat: 10.252338482105115, lng: 124.01940599374602 };
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//       zoom: 10,
+//       center: center
+//   });
+
+//   var marker = new google.maps.Marker({
+//       position: center,
+//       map: map
+//   });
+
+//   getLocationData('Cebu, Philippines');
+
+//   function getLocationData(location) {
+//     const apiKey = 'AIzaSyCAC8eP20iVmcCd6y-W3WTnCEgZmLId0M0';
+//     const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=${apiKey}`;
+  
+// }}
+
+
+// dark mode
+// Add this script in your JavaScript file or within <script> tags in your HTML
+
+// Function to toggle dark mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
