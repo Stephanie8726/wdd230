6c84cecb36d16b776f4f356515ac7d14caeccb1f
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2024-03-26 14:47:34 -0800

    final changes

--- chamber/scripts/common-functions.js
+++ chamber/scripts/common-functions.js
@@ -23,14 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const main = document.querySelector("main");
   
     modeButton.addEventListener("click", () => {
-        if (modeButton.textContent.includes("☀")) {
+        if (modeButton.textContent.includes("🌑")) {
             main.style.background = "#000";
             main.style.color = "#fff";
             modeButton.textContent = "🔆";
         } else {
             main.style.background = "#eee";
             main.style.color = "#000";
-            modeButton.textContent = "☀";
+            modeButton.textContent = "🌑";
         }
     });
   
